cd01ca842365ab38d48e307ab470f63c1b411db0
shalaik <shalaikooleksandr@gmail.com>
2019-08-13 11:37:39 +0300

    first project-commit

---
+++ js1.js
@@ -0,0 +1,44 @@
+//let a=prompt("Введіть А");
+// let b=prompt("Введіть B");
+
+//if (a>b) {
+//          alert(a);
+//         }
+//lse if (a<b) {
+//               alert(b);
+//              }
+// else {
+//      alert(a+'='+b);
+//     }
+
+//if ( (a>10 || b <=12) && b>a)
+//{
+//    alert("ура");
+//}
+//else
+//{
+//    alert("увы");
+//
+//
+// }
+
+let a = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+i = 0;
+
+for(i=0;i<10;i++)
+   {
+       console.log(a[i]);
+   }
+
+i=0;
+do {
+    console.log(a[i]);
+    i++;
+}
+while (i < 10);
+
+i = 0;
+while (i < 10) {
+    console.log(a[i]);
+    i++;
+}
